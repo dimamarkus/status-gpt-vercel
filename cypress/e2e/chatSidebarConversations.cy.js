2c49cdbd1b5c70d64f21f069d70b4f975fd0beab
wwakabobik <i.vereshchagin@gmail.com>
2023-05-10 17:10:01 +0200

    More tests + fixes

--- cypress/e2e/chatSidebarConversations.cy.js
+++ cypress/e2e/chatSidebarConversations.cy.js
@@ -55,6 +55,15 @@ describe("Testing Conversations", () => {
       conversation_items[0].cancelButton.should("not.exist");
       conversations.clearButton.should("be.visible");
       conversations.zeroState.should("not.exist");
+      conversations.container.should("be.visible");
+      conversations.menu.should("be.visible");
+      conversations.addConversationButton.should("be.visible");
+      conversations.addFolderButton.should("be.visible");
+      conversations.filterInput.should("not.exist");
+      conversations.clearButton.should("be.visible");
+      conversations.importButton.should("be.visible");
+      conversations.exportButton.should("be.visible");
+      conversations.zeroState.should("not.exist");
     });
   });
 
@@ -124,7 +133,7 @@ describe("Testing Conversations", () => {
 
   it("Add several conversations", () => {
     openedConversations();
-    const conversationsCount = Math.floor(Math.random() * 5) + 1;
+    const conversationsCount = Math.floor(Math.random() * 4) + 2;
     for (let i = 0; i < conversationsCount; i++) {
       conversations.addConversation();
     }
@@ -136,18 +145,16 @@ describe("Testing Conversations", () => {
 
   const testCases = [
     ["PATTERN", Math.floor(Math.random() * 4) + 2],
-    [`_${Cypress._.random(1, Math.floor(Math.random() * 4) + 2)}`, 1],
+    [0, 1],
     [randomString(), 0],
   ];
 
   Cypress._.each(testCases, ([pattern, expectedCount]) => {
-    it("Conversations filtering", () => {
+    it.only("Conversations filtering", () => {
       openedConversations();
-      let conversationsCount;
-      if (expectedCount < 2) {
-        conversationsCount = Math.floor(Math.random() * 4) + 2;
-      } else {
-        conversationsCount = expectedCount;
+      let conversationsCount = Math.floor(Math.random() * 4) + 2;
+      if (pattern == 0) {
+        pattern = `_${Cypress._.random(2, conversationsCount)}`;
       }
       for (let i = 0; i < conversationsCount; i++) {
         conversations.addConversation();
@@ -167,15 +174,58 @@ describe("Testing Conversations", () => {
   });
 
   it("Delete conversation [cancel]", () => {
-    // TBD
+    openedConversations();
+    conversations.addConversation();
+    conversations.getAllItems().then((conversation_items) => {
+      conversation_items[0].deleteButton.should("be.visible");
+      conversation_items[0].deleteButton.click();
+      conversation_items[0].cancelButton.should("be.visible");
+      conversation_items[0].cancelButton.click();
+      conversation_items[0].cancelButton.should("not.exist");
+      conversations.getCount().then((conversationsActualCount) => {
+        expect(conversationsActualCount).to.equal(1);
+      });
+    });
   });
 
-  it("Delete conversation [submit]", () => {
-    // TBD
+  it("Delete conversation [last]", () => {
+    openedConversations();
+    conversations.addConversation();
+    conversations.getAllItems().then((conversation_items) => {
+      conversation_items[0].deleteButton.should("be.visible");
+      conversation_items[0].deleteButton.click();
+      conversation_items[0].confirmButton.should("be.visible");
+      conversation_items[0].confirmButton.click();
+      conversation_items[0].confirmButton.should("not.exist");
+      conversations.zeroState.should("be.visible");
+      conversations.getCount().then((conversationsActualCount) => {
+        expect(conversationsActualCount).to.equal(0);
+      });
+    });
   });
 
-  it("Delete conversation [cancel]", () => {
-    // TBD
+  it("Delete conversation [several]", () => {
+    openedConversations();
+    const conversationsCount = Math.floor(Math.random() * 5) + 1;
+    for (let i = 0; i < conversationsCount; i++) {
+      conversations.addConversation();
+    }
+    for (let i = 0; i < conversationsCount; i++) {
+      conversations.getAllItems().then((conversation_items) => {
+        let any_index = Cypress._.random(0, Math.floor(Math.random() * (conversationsCount - i)));
+        console.log("any_index:", any_index);
+        conversation_items[any_index].container.click();
+        conversation_items[any_index].deleteButton.should("be.visible");
+        conversation_items[any_index].deleteButton.click();
+        conversation_items[any_index].confirmButton.should("be.visible");
+        conversation_items[any_index].confirmButton.click();
+        conversation_items[any_index].confirmButton.should("not.exist");
+        conversations.getCount().then((conversationsActualCount) => {
+          expect(conversationsActualCount).to.equal(conversationsCount - i - 1);
+        });
+      });
+    }
+    conversations.zeroState.should("be.visible");
   });
 
   it("Import conversations", () => {
